aae97b2dc2d807c2b9c7b642c83e588aae6a96cb
Alexander Potashev <aspotashev@gmail.com>
2012-08-11 13:00:19 +0400

    Create the initial .pot file also in $podir
    Otherwise the output of "kdegettext.rb" will be stacked in the .pot
    in multiple copies.
    
    Thanks to Yuri Chornoivan for reporting the problem.
    
    CCMAIL: robert.riemann@physik.hu-berlin.de
    CCMAIL: yurchor@ukr.net
    CCMAIL: kde-i18n-doc@kde.org

--- Messages.sh
+++ Messages.sh
@@ -5,7 +5,7 @@ set -o errexit
 # have to unset LANGUAGE as a work-around of a ruby-locale bug
 # http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=670320
 unset LANGUAGE
-cat > kubeplayer.pot <<HEADER
+cat > $podir/kubeplayer.pot <<HEADER
 # Language template for Kubeplayer
 #
 # Copyright (C) 2012
